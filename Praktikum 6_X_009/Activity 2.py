3521e48dc6ea4aa9d58ef04b07ce599cb0d10753
L200194009 <55549008+L200194009@users.noreply.github.com>
2019-11-06 20:49:12 +0700

    Add files via upload

---
+++ Praktikum 6_X_009/Activity 2.py
@@ -0,0 +1,8 @@
+import random
+angka = random.randint(0,1000)
+
+Nama = "Nadia Zerlinda Taufik Putri"
+TanggalLahir = "23 April 2001"
+NamaSingkat = Nama[0:15] + Nama[15] + "." + Nama[22] + "."
+Ussername = Nama[0] + TanggalLahir[0] + TanggalLahir[9:13]
+Password = Nama[0:4] + str(angka)
