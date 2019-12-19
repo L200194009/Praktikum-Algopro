ddac5196522de03c45d88e85afa202d05571a5e2
L200194009 <55549008+L200194009@users.noreply.github.com>
2019-12-19 19:40:27 +0700

    Delete index.py

--- Praktikum12/L200194009/cgi-bin/index.py
+++
@@ -1,9 +0,0 @@
-print("<!DOCTYPE html>\n")
-print("""<html>
-<head><title>Akses dengan SimpleHTTPRequestHadler</title></head>
-<body><h3>Data Diri Mahasiswa</h3>""")
-print("""<table><tr><td>Nama</td><td>:</td><td>Nadia Zerlinda</td></tr>
-		<tr><td>Tempat, tanggal lahir</td><td>:</td><td>Tulungagung,23 April 2001</td></tr>
-		<tr><td>Tempat wisata favorit</td><td>:</td><td>Pantai</td></tr>
-		<tr><td>Motto</td><td>:</td><td>Happy ending</td></tr></table>""")
-print("</body></html>")
